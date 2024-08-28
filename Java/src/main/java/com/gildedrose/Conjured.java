30131114e8ed83b14d1ab7ca51a78cf783387fc9
Toad <tran.theodore@gmail.com>
2024-08-28 23:24:31 +0200

    Added nominal test unit

--- Java/src/main/java/com/gildedrose/Conjured.java
+++ Java/src/main/java/com/gildedrose/Conjured.java
@@ -4,7 +4,7 @@ public class Conjured implements ItemInterface{
     @Override
     public void updateItem(Item item) {
         if (item.sellIn<=0){item.quality -= 2;}else item.quality--;
-        if (item.quality < 0) {item.quality = 0;}
         item.sellIn--;
+        if (item.quality < 0) {item.quality = 0;}
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,12 +7,122 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void testCommons() {
-        Item[] items = new Item[] { new ItemBuilder().build("+5 Dexterity Vest",10,20) };
+    void agedbrie_increase_quality_by1_at_endofday(){
+        Item[] items = new Item[] { new ItemBuilder().build("Aged Brie",2,0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("+5 Dexterity Vest", app.items[0].name);
+        assertEquals(1,app.items[0].sellIn);
+        assertEquals(1,app.items[0].quality);
+    }
+    @Test
+    void agedbrie_never_increase_quality_above_50(){
+        Item[] items = new Item[] { new ItemBuilder().build("Aged Brie",2,50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1,app.items[0].sellIn);
+        assertEquals(50,app.items[0].quality);
+    }
+    @Test
+    void agedbrie_minimal_quality_equals_0() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Backstage passes to a TAFKAL80ETC concert",50,-1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(49,app.items[0].sellIn);
+        assertEquals(0,app.items[0].quality);
+    }
+
+    @Test
+    void backstagepass_increase_quality_by1_at_endofday(){
+        Item[] items = new Item[] { new ItemBuilder().build("Backstage passes to a TAFKAL80ETC concert",20,1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(19,app.items[0].sellIn);
+        assertEquals(2,app.items[0].quality);
+    }
+    @Test
+    void backstagepass_increase_quality_by2_when_sellIn_between_10and5days(){
+        Item[] items = new Item[] { new ItemBuilder().build("Backstage passes to a TAFKAL80ETC concert",10,1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
         assertEquals(9,app.items[0].sellIn);
-        assertEquals(19,app.items[0].quality);
+        assertEquals(3,app.items[0].quality);
+    }
+
+    @Test
+    void backstagepass_increase_quality_by3_when_sellIn_between_5and0days() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Backstage passes to a TAFKAL80ETC concert",5,1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4,app.items[0].sellIn);
+        assertEquals(4,app.items[0].quality);
+    }
+    @Test
+    void backstagepass_increase_quality_by3_when_sellIn_equalsTo_0() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Backstage passes to a TAFKAL80ETC concert",1,1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0,app.items[0].sellIn);
+        assertEquals(4,app.items[0].quality);
+    }
+    @Test
+    void backstagepass_quality_setTo_0_when_sellIn_lesserThan_0() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Backstage passes to a TAFKAL80ETC concert",0,50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1,app.items[0].sellIn);
+        assertEquals(0,app.items[0].quality);
+    }
+    @Test
+    void backstagepass_minimal_quality_equals_0() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Backstage passes to a TAFKAL80ETC concert",50,-1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(49,app.items[0].sellIn);
+        assertEquals(0,app.items[0].quality);
+    }
+
+    @Test
+    void sulfuras_quality_never_decrease() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Sulfuras, Hand of Ragnaros",0,80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0,app.items[0].sellIn);
+        assertEquals(80,app.items[0].quality);
+    }
+
+    @Test
+    void sulfuras_sellIn_never_changes() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Sulfuras, Hand of Ragnaros",99,80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(99,app.items[0].sellIn);
+        assertEquals(80,app.items[0].quality);
+    }
+
+    @Test
+    void conjured_decrease_quality_by2_when_sellIn_EqualsOrLesserThan_0() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Conjured Mana Cake",0,50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1,app.items[0].sellIn);
+        assertEquals(48,app.items[0].quality);
+    }
+
+    @Test
+    void conjured_minimal_quality_equals_0() {
+        Item[] items = new Item[] {
+            new ItemBuilder().build("Conjured Mana Cake",0,1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1,app.items[0].sellIn);
+        assertEquals(0,app.items[0].quality);
     }
 }
